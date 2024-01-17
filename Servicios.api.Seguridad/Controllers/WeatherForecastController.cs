8ed56c1883bf8c9c203b9920a225f5fddf52d617
wilmar.martinez <wilmar.martinez@WILMAR-MARTINEZ>
2024-01-17 14:17:55 -0500

    Cambio en controller

--- Servicios.api.Seguridad/Controllers/WeatherForecastController.cs
+++ Servicios.api.Seguridad/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Servicios.api.Seguridad.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hot"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
